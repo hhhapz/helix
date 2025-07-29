4550ba45f921805a6fc77ed39fa9dea8ab8d8576
s0LA1337 <dream@neoncity.dev>
2025-07-29 14:59:59 -0700

    Add rainbow indentation guides

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -932,11 +932,20 @@ impl Default for WhitespaceCharacters {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum RainbowIndent {
+    None,
+    Dim,
+    Normal,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(default, rename_all = "kebab-case")]
 pub struct IndentGuidesConfig {
     pub render: bool,
     pub character: char,
     pub skip_levels: u8,
+    pub rainbow_indent: RainbowIndent,
 }
 
 impl Default for IndentGuidesConfig {
@@ -945,6 +954,7 @@ impl Default for IndentGuidesConfig {
             skip_levels: 0,
             render: false,
             character: '│',
+            rainbow_indent: RainbowIndent::None,
         }
     }
 }
